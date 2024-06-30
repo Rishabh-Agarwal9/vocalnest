6d478038ada338db7e53064f465bdc38de01c9ec
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-07-01 01:50:43 +0530

    added user auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+import PodcastCard from '@/components/PodcastCard'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import { podcastData } from '@/constants'
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+
 
 const Home = () => {
+  // const tasks = useQuery(api.tasks.get);
   return (
     <div className='mt-9 flex-col gap-9'>
       <section className='flex flex-col gap-5'>
         <h1 className='text-20 font-bold text-white-1'>Trending podcasts</h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+          {/* {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)} */}
+        </div>
         <Button className='text-white-1 bg-orange-1'>Button</Button>
+        <div className='podcast_grid'>
+          {podcastData.map(({ id, title, description, imgURL })=>(
+            <PodcastCard 
+            key={id}
+            imgUrl={imgURL}
+            title={title}
+            description={description}
+            podcastId={id}
+          />
+          ))}
+        </div>
+        
       </section>
     </div>
   )
