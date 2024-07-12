72e1e4caed01586a62583129822887d8e15803f3
Rishabh-Agarwal9 <rishabha1233@gmail.com>
2024-07-12 19:44:21 +0530

    added files

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -22,7 +22,7 @@ const LeftSidebar = () => {
       'h-[calc(100vh-140px)]': audio?.audioUrl
     })}>
       <nav className="flex flex-col gap-6">
-        <Link href="/" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
+        <Link href="/about" className="flex cursor-pointer items-center gap-1 pb-10 max-lg:justify-center">
           <Image src="/icons/logo.svg" alt="logo" width={23} height={27} />
           <h1 className="text-24 font-extrabold text-white max-lg:hidden">VocalNest</h1>
         </Link>
